82c067c4e63f378f627601ce11d814a494dbdaaf
Zachary Starkey <zstarkey@icloud.com>
2021-04-13 18:46:22 -0400

    Added ability to spawn two spikes together at times

--- Assets/Scripts/Game_Scripts/Spike_Generation.cs
+++ Assets/Scripts/Game_Scripts/Spike_Generation.cs
@@ -11,26 +11,39 @@ public class Spike_Generation : MonoBehaviour
     public GameObject MinSpawnLocObj;
     public float MinSpacingBetween;
     public float MaxSpacingBetween;
-    public float AmountofSpikes;
+    public int AmountofSpikes;
 
     private Vector3 MinSpawnLocation;
     private Vector3 MaxSpawnLocation;
     // Start is called before the first frame update
     void Start()
     {
+        //get the min and max vectors of where to spawn the spikes
         MinSpawnLocation = MinSpawnLocObj.transform.position;
         MaxSpawnLocation = MaxSpawnLocObj.transform.position;
+        //the location the spike obj will spawn at
         Vector3 SpawnLoc = MinSpawnLocation;
+        //do this for how ever many spikes we want
         for (int i =0; i < AmountofSpikes; i++)
         {
+            int NumberSpikes = 0;
+            //if our spikes z position value goes past the max then we spawn one more spike and then stop spawning them
             if (SpawnLoc.z > MaxSpawnLocation.z)
             {
                 SpawnLoc.z = MaxSpawnLocation.z;
                 Instantiate(SpikeObject, SpawnLoc, SpikeObject.transform.rotation);
                 break;
             }
-            Instantiate(SpikeObject, SpawnLoc, SpikeObject.transform.rotation);
-            SpawnLoc.z += Random.Range(MinSpacingBetween, MaxSpacingBetween);
+            NumberSpikes = Random.Range(1, 3);// get either 1 or 2
+            //if we are spawning two spikes together then we spawn the first one andd the offset fo the second one
+            //else we just spawn one spike at the location
+            if (NumberSpikes == 2)
+            {
+                Instantiate(SpikeObject, SpawnLoc, SpikeObject.transform.rotation);//spawn the spike obj at certain location and rotation
+                SpawnLoc.z += 1.0f;
+            }
+            Instantiate(SpikeObject, SpawnLoc, SpikeObject.transform.rotation);//spawn the spike obj at certain location and rotation
+            SpawnLoc.z += Random.Range(MinSpacingBetween, MaxSpacingBetween);//offset the next spike location by a random amount
         }
     }
 }
